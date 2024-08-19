be06d0bf06bb79772b7cf56d77540919e0719b28
chenhao5 <chenhao5@jianke.com>
2024-08-19 14:54:18 +0800

    fixbugs

--- AutoFix.py
+++ AutoFix.py
@@ -17,22 +17,26 @@ try:
     res = requests.get('https://gitee.com/if-the-wind/github-hosts/raw/main/hosts', headers=headers)
     for line in res.text.split('\n'):
         if 'github' in line and line[0] != '#':
-            url = line.split(' ')[-1]
-            ip = line.split(' ')[0]
+            url = line.split(' ')[-1].strip()
+            ip = line.split(' ')[0].strip()
             with open(host_path, 'r') as f:
                 c = f.readlines()
-            switch = False
-            for i, l in enumerate(c):
-                if url == l.split(' ')[-1]: 
-                    c[i] = '\n' + ip +' ' + url
-                    print('inserted '+ line +' to '+ host_path)
-                    switch = True       
-                    break
-            if i == len(c) - 1 and switch == False:
+            if not any(url in l for l in c):
                 c.append('\n' + ip +' ' + url)
-                print('inserted '+ line +' to '+ host_path)
-            with open(host_path, 'w') as f:
-                f.writelines(c)
+                print('inserted2 '+ line +' to '+ host_path)
+                with open(host_path, 'w') as f:
+                    f.writelines(c)
+                logging.info('inserted '+ line +' to '+ host_path)
+            else:
+                for i, l in enumerate(c):
+                    if url == l.split(' ')[-1].strip() and line[0] != '#': 
+                        c[i] = '\n' + ip +' ' + url
+                        print('refreshed '+ line +' to '+ host_path)
+                        break   
+                    else:
+                        continue
+                with open(host_path, 'w') as f:
+                    f.writelines(c)
             logging.info('inserted '+ line +' to '+ host_path)
 except Exception as e:
     logging.error(e)
